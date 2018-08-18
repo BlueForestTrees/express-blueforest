eedcffc2dbfcc7d2d0f6ef83a069145c486cf3cf
slim <smedini@gmail.com>
2018-08-18 20:53:45 +0200

    web: imports, tree: ressource, branche, impacts. imports: mapper l'unité de référence ademe sur celle blueforest

--- src/run.js
+++ src/run.js
@@ -1,7 +1,7 @@
-import errors from "./errors";
+import errors from "./errors"
 
-const {validationResult} = require('express-validator/check');
-const {matchedData} = require('express-validator/filter');
+const {validationResult} = require('express-validator/check')
+const {matchedData} = require('express-validator/filter')
 
 export default (work, workname) => (req, res, next) =>
     Promise
@@ -10,19 +10,21 @@ export default (work, workname) => (req, res, next) =>
 
 const doWork = async (req, res, next, work, workname) => {
     if (!res.locals.validated) {
-        res.locals.validated = true;
+        res.locals.validated = true
         res.locals.result = validate(req, res)
         console.log("INPUT", JSON.stringify(res.locals.result, null, 2))
     }
     res.locals.result = await work(res.locals.result, req, res, next)
-    console.log((workname || "WORK") + " RESULT", JSON.stringify(res.locals.result, null, 2))
-    next();
-};
+    if (workname) {
+        console.log(workname + " RESULT", JSON.stringify(res.locals.result, null, 2))
+    }
+    next()
+}
 
 const validate = req => {
-    const validationErrors = validationResult(req);
+    const validationErrors = validationResult(req)
     if (!validationErrors.isEmpty()) {
-        throw new errors.ValidationError(validationErrors.mapped());
+        throw new errors.ValidationError(validationErrors.mapped())
     }
     return matchedData(req)
-};
+}
