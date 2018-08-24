5c58d06ddd8324df8c29611f048c85c79515735f
slim <smedini@gmail.com>
2018-08-24 18:22:00 +0200

    usage de debug

--- src/express-blueforest.js
+++ src/express-blueforest.js
@@ -1,81 +1,82 @@
-import express from 'express';
-import path from "path";
-import bodyParser from 'body-parser';
-import read from 'fs-readdir-recursive';
-import _run from "./run";
-import _errors from "./errors";
+import express from 'express'
+import path from "path"
+import bodyParser from 'body-parser'
+import read from 'fs-readdir-recursive'
+import _run from "./run"
+import _errors from "./errors"
+import morgan from "morgan"
 
-export const Router = require("express").Router;
-export const run = _run;
-export const convert = _run;
-export const errors = _errors;
+const debug = require('debug')('api:express')
+const error = require('debug')('api:express:err')
+export const Router = require("express").Router
+export const run = _run
+export const convert = _run
+export const errors = _errors
 
-export default (ENV, errorAdapter, init) => () => {
-    const port = ENV.PORT || 8080;
-    console.log("starting express on" , port);
-    const api = express();
-
-    if (init) {
-        init(api)
-    }
-
-    api.use(bodyParser.json());
-    api.use(bodyParser.urlencoded({extended: false}));
-
+export default (ENV, errorAdapter) => () => {
+    const port = ENV.PORT || 8080
+    debug("starting on %o", port)
+    const api = express()
+    
+    api.use(morgan(ENV.MORGAN || ':status :method :url :response-time ms - :res[content-length]', { stream: { write: msg => debug(msg) } }))
+    api.use(bodyParser.json())
+    api.use(bodyParser.urlencoded({extended: false}))
+    
     //LOG REQ
     api.use(function (req, res, next) {
-        console.log("HTTP REQUEST", JSON.stringify({
-            url: `${req.method} ${req.url}`,
-            body: req.body
-        }, null, 2))
-        next();
-    });
-
+        if (debug.enabled) {
+            debug("HTTP REQUEST BODY %o", req.body)
+        }
+        next()
+    })
+    
     //REST
     let restPath = path.resolve(ENV.REST_PATH)
-    console.log("scanning rest services @", restPath);
-    let count = 0;
+    debug("scanning rest services @%o", restPath)
+    let count = 0
     read(restPath).forEach(function (file) {
         const p = path.join(restPath, file)
         try {
             file.indexOf("Rest.js") > 1 && api.use(require(p))
             count++
         } catch (e) {
-            console.error("erreur au chargement du rest service", p, e)
+            error("erreur au chargement du rest service", p, e)
         }
-    });
+    })
     if (count > 0) {
-        console.log(`${count} rest services`)
+        debug('%o rest services', count)
     } else {
-        console.error("pas de rest service!")
+        error("pas de rest service!")
     }
-
+    
     //RESPONSE TO RETURN
     api.use(function (req, res, next) {
         if (res.locals.result !== undefined) {
-            console.log("HTTP RESPONSE BODY", JSON.stringify(res.locals.result, null, 2))
-            res.json(res.locals.result);
+            if (debug.enabled) {
+                debug("HTTP RESPONSE BODY %o", res.locals.result)
+            }
+            res.json(res.locals.result)
         } else {
-            next();
+            next()
         }
-    });
-
+    })
+    
     //NOT FOUND
     api.use(function (req, res, next) {
-        const err = new Error();
-        err.status = 404;
-        next(err);
-    });
-
+        const err = new Error()
+        err.status = 404
+        next(err)
+    })
+    
     //ERROR
     api.use(function (err, req, res, next) {
-
+        
         if (errorAdapter) {
-            errorAdapter(err);
+            errorAdapter(err)
         }
-
-        res.status(err.status || 500);
-        let responseBody = null;
+        
+        res.status(err.status || 500)
+        let responseBody = null
         if (err.body) {
             responseBody = err.body
         } else if (err.errorCode && err.message) {
@@ -83,13 +84,13 @@ export default (ENV, errorAdapter, init) => () => {
         } else if (err.errorCode) {
             responseBody = {errorCode: err.errorCode}
         }
-        res.json(responseBody);
-        console.error("res error", responseBody);
-        console.error("error", err);
-    });
-
+        res.json(responseBody)
+        error("response %o", responseBody)
+        error("error %o", err)
+    })
+    
     //LISTENING
-    const server = api.listen(port);
-    console.log("started");
-    return server;
+    const server = api.listen(port)
+    debug("started")
+    return server
 };
--- src/run.js
+++ src/run.js
@@ -1,5 +1,6 @@
 import errors from "./errors"
 
+const debug = require('debug')('api:express')
 const {validationResult} = require('express-validator/check')
 const {matchedData} = require('express-validator/filter')
 
@@ -12,11 +13,11 @@ const doWork = async (req, res, next, work, workname) => {
     if (!res.locals.validated) {
         res.locals.validated = true
         res.locals.result = validate(req, res)
-        console.log("INPUT", JSON.stringify(res.locals.result, null, 2))
+        debug("WORK INPUT %o", res.locals.result)
     }
     res.locals.result = await work(res.locals.result, req, res, next)
     if (workname) {
-        console.log(workname + " RESULT", JSON.stringify(res.locals.result, null, 2))
+        debug("WORK %o:  %o", workname, res.locals.result)
     }
     next()
 }
